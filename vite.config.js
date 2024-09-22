1342602a21e82c2c7e3e8a6119a5ed01d3f1876a
Sarah Oliveira <soliveirarm@gmail.com>
2024-09-22 19:12:15 -0300

    feat: turn site into a pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Teacher's Salary",
+        description:
+          "App criado pra calcular o salário de professores de acordo com a hora aula",
+        display: "fullscreen",
+        background_color: "##f8fafc",
+        theme_color: "#3B82F6",
+        icons: [
+          {
+            src: "favicon.svg",
+            sizes: "192x192",
+            type: "image/svg",
+          },
+          {
+            src: "favicon.svg",
+            sizes: "512x512",
+            type: "image/svg",
+          },
+        ],
+      },
+    }),
+  ],
 })
